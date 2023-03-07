697bce7405d237b6d23d3cc10a6c7e0f9857047e
Rifqi <mohamadrifki66@gmail.com>
2023-03-07 10:13:43 +0700

    challange6 selesai

--- c6/challange6.js
+++ c6/challange6.js
@@ -1,41 +1,25 @@
-function sentencesManipulation(sentence) {
-  let vokal = ['a', 'i', 'u', 'e', 'o'];
-  let variabel = sentence.charAt(0)
-   
-  if  (variabel == vokal[0]) {
-    return sentence
-  } else if  (variabel == vokal[1]){
-    return sentence
-  } else if  (variabel == vokal[2]){
-    return sentence
-  } else if  (variabel == vokal[3]){
-    return sentence
-  } else if  (variabel == vokal[4]){
-  } else {    
-    return sentence.substr(1) + variabel + 'nyo'
+ function sentencesManipulation(sentence) {
+   let vokal = ['a', 'i', 'u', 'e', 'o'];
+   let kalimat  = sentence.split(' ');
+
+   let text = [];
+
+   for (let i = 0; i < kalimat.length; i++) {
+
+     if  (kalimat[i].charAt(0) == vokal[0]) {
+       text.push(kalimat[i])
+     } else if  (kalimat[i].charAt(0) == vokal[1]){
+       text.push(kalimat[i])
+     } else if  (kalimat[i].charAt(0) == vokal[2]){
+       text.push(kalimat[i])
+     } else if  (kalimat[i].charAt(0) == vokal[3]){
+       text.push(kalimat[i]) 
+     } else if  (kalimat[i].charAt(0) == vokal[4]){
+     } else {
+       let konsonan = kalimat[i].substring(1) + kalimat[i].charAt(0) + 'nyo'
+       text.push(konsonan)
+     }
+   }
+   console.log(text.join(' '));
  }
-};
-console.log(sentencesManipulation('ibu'),sentencesManipulation('pergi'),sentencesManipulation('ke'),sentencesManipulation('pasar'), sentencesManipulation('bersama'),sentencesManipulation('aku'))
-
-
-
-/*function sentencesManipulation(sentence) {
-  let variasi = sentence.charAt(0)
-  let huruf = sentence[0]
-       
-  switch (huruf) {
-      case "a":
-      case "i" :
-      case "u" :
-      case "e" :
-      case "o" :
-          return sentence;
-          break;
-      default:
-         console.log((sentence.substr(1) + variasi + 'nyo'))
-         break;
-  }
-
-};
-
-sentencesManipulation('ibu pergi ke pasar bersama aku')*/
+sentencesManipulation('ibu pergi ke pasar bersama aku')
