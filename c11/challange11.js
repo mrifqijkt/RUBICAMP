e7bef946036a0d9e549a9438c49e76d83f1d5a2c
Rifqi <mohamadrifki66@gmail.com>
2023-03-17 19:37:12 +0700

    challange11 selesai

--- c11/challange11.js
+++ c11/challange11.js
@@ -1,26 +1,38 @@
 const readline = require('readline');
 
-const fs = require('fs')
-const content = fs.readFileSync('data.json')
-const object = JSON.parse(content)
+const fs = require('fs');
+const content = fs.readFileSync('data.json');
+const obj = JSON.parse(content);
 
-
+let i = 0;
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-    prompt: 'Tebakan> ',
+    prompt: 'Tebakan > '
 });
+
 function tebakKata() {
-    console.log('Selamat datang di permainan tebak kata,silahkan isi dengan jawaban yang benar ya!');
-    console.log(`Pertanyaan:${object.definiton}`);
+    console.log('Selamat datang di permainan tebak kata, silahkan isi dengan jawaban yang benar ya! ');
+    console.log(`Pertanyaan: ${obj[i].definition}`);
     rl.prompt();
-    rl.on('line',function(answer){
-        if(answer == object.term){
-            console.log('Jawaban anda benar!')
+    rl.on('line', function (answer) {
+        if (answer.toLowerCase() == obj[i].term) {
+            console.log('Jawaban anda benar')
+            i++;
+
+            if (i == obj.length) {
+                console.log('Selamat anda jadi Pemenang!!!');
+                process.exit();
+            }
+            console.log("Pertayaan: " + obj[i].definition);
+            rl.prompt();
         }
-    })
+        else {
+            console.log('Wkwkwkwk anda kurang beruntung');
+            rl.prompt();
+        }
+    });
+
 }
-
-
 
 tebakKata()
