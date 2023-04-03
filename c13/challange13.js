47f738fbe8f563f8825a7e55b0e2ee06ca41677d
Rifqi <mohamadrifki66@gmail.com>
2023-04-03 13:31:32 +0700

    blom selesai

--- c13/challange13.js
+++ c13/challange13.js
@@ -1,35 +1,13 @@
-//  const fs = require('fs');
-//  const process = require('process');
 
-//  const data = fs.readFileSync('data.json');
-//  const taskData = JSON.parse(data);
-
- function addTask (){
-     taskData.push({"task_id":null,"task_content":content,"status":false,"tag":[]})
-     
-     fs.writeFileSync(`data.json`,JSON.stringify(taskData, null,content,false,[]))
- }
-
-
- console.log(`>>> JS TODO <<<
- $ node todo.js list
- $ node todo.js <command>
- $ node todo.js task <task_id>
- $ node todo.js add <task_content>
- $ node todo.js delete <task_id>
- $ node todo.js <task_id>
- $ node todo.js uncomplete <task_id>
- $ node todo.js list:outstanding asc|desc
- $ node todo.js list:completed asc|desc
- $ node todo.js tag <task_id> <tag_name_1> <tag_name_2> ... <tag_name_N>
- $ node todo.js filter:<tag_name>`)
-
-//  switch (command) {
-//      case "list":
-//          console.log("Daftar Pekerjaan")
-//          break;
-
-//      default:
-//          break;
-//  }
-
+        console.log(">>> JS TODO <<<")
+        console.log(` <command>`);
+        console.log(` list`);
+        console.log(` task `);
+        console.log(` add <task_content>`);
+        console.log(` delete `);
+        console.log(` complete `);
+        console.log(` uncomplete `);
+        console.log(` list:outstanding asc|desc`);
+        console.log(` list:completed asc|desc`);
+        console.log(` tag  <tag_name_1> <tag_name_2> ... <tag_name_N>`);
+        console.log(` filter:<tag_name>`);
