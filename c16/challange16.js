6e08bff9de89a9d6a744db76e6fcf36aa9811c69
Rifqi <mohamadrifki66@gmail.com>
2023-05-16 20:24:02 +0700

    challange 16 selesai

--- c16/challange16.js
+++ c16/challange16.js
@@ -1,86 +1,39 @@
-// class Car {
-//     constructor(varian, door, seat, tyre, year, warranty) {
-//         this.varian = varian
-//         this.door = door
-//         this.seat = seat
-//         this.tyre = tyre
-//         this.year = year
-//         this.warranty = warranty
-
-//     }
-
-// }
-
-// class Tyre {
-//     constructor(brand, size) {
-//         this.bran = brand
-//         this.size = size
-
-//     }
-
-// }
-
-
-// class CarFactory {
-//     constructor() {
-//         this.cars = []
-//     }
-
-//     produce(year) {
-//         console.log(`mobil${this.varian}produce 2020`)
-
-//     }
-
-//     guaranteeSimulation(simulationYear) {
-
-//     }
-// }
-
-// const toyota = new CarFactory()
-// toyota.produce(2020)
-// toyota.produce(2022)
-// toyota.result()
-// toyota.guaranteeSimulation(2025)
-
-// const agya = new Car('agya', 5, 5,'dunlop' , 2020, 1)
-
-// console.log(agya)
 class Car {
-    constructor(mesin, warna, pintu, merkban, tahun, garansi) {
-        this.mesin = mesin;
-        this.warna = warna;
-        this.pintu = pintu;
-        this.ban = new Tyre(merkban);
-        this.tahun = tahun;
-        this.garansi = garansi;
+    constructor(varian, door, seat, tyre, year, warranty) {
+        this.varian = varian;
+        this.door = door;
+        this.seat = seat;
+        this.year = year;
+        this.tyre = tyre;
+        this.warranty = warranty;
     }
 }
 
 class Tyre {
-    constructor(merk, size) {
-        this.merk = merk;
-        this.size = size || 17;
+    constructor(brand, size) {
+        this.brand = brand;
+        this.size = size;
     }
 }
 
 class Agya extends Car {
-    constructor(tahun) {
-        super('1500cc', 'silver', 4, 'Dunlop', tahun, 3);
+    constructor(year) {
+        super('Agya', 5, 5, 'dunlop 15 inch', year, '1 year');
         this.type = 'Agya';
     }
 }
 
-class Avanza extends Car {
-    constructor(tahun) {
-        super('1300cc', 'hitam', 4, 'Michelin', tahun, 5);
-        this.type = 'Avanza';
+class Rush extends Car {
+    constructor(year) {
+        super('Rush', 5, 5, 'bridgestone 17 inch', year, '3 years');
+        this.type = 'Rush';
     }
 }
 
-class Rush extends Car {
-    constructor(tahun) {
-        super('2500cc', 'putih', 4, 'Bridgestone', tahun, 5);
-        this.type = 'Rush';
+class Alya extends Car {
+    constructor(year) {
+        super('Alya', 5, 5, 'michellin 15 inch', year, '5 years');
+        this.type = 'Alya';
     }
 }
 
@@ -98,10 +51,10 @@ class CarFactory {
             this.cars.push(new Agya(tahun));
         }
         for (let i = 0; i < CarFactory.acak(); i++) {
-            this.cars.push(new Avanza(tahun));
+            this.cars.push(new Rush(tahun));
         }
         for (let i = 0; i < CarFactory.acak(); i++) {
-            this.cars.push(new Rush(tahun));
+            this.cars.push(new Alya(tahun));
         }
     }
 
@@ -110,14 +63,16 @@ class CarFactory {
         this.cars.forEach((content) => {
             console.log(`
             Nama Mobil: ${content.type}
-            Warna: ${content.warna}
-            Kapasitas Mesin: ${content.mesin}
-            Jumlah Pintu: ${content.pintu}
-            Merk Ban: ${content.ban.merk}
-            Tahun Pembuatan: ${content.tahun}
-            Garansi: ${content.garansi} Tahun
-            Masa berlaku garansi: ${year - content.tahun > content.garansi ? 'Tidak berlaku' : 'Masih berlaku'}
-            ===================================
+            varian: ${content.varian}
+            door: ${content.door}
+            seat: ${content.seat}
+            tyre: ${content.tyre}
+            year: ${content.year} 
+            warranty: ${content.warranty} 
+            Masa berlaku garansi: ${
+                year - content.year > parseInt(content.warranty) ? 'Tidak berlaku' : 'Masih berlaku'
+            }
+            
             `);
         });
     }
